b5cf32bf500c54c7b6ae02e4d1511bc4087a28d3
MINSUNG KIM <96581415+6sung@users.noreply.github.com>
2024-04-11 17:06:01 +0900

    Add files via upload

---
+++ PRACTICE/HTMLPrj/src/main/webapp/js_example/js/first.js
@@ -0,0 +1,10 @@
+function hello(name){
+	console.log("Hello~ "+name);
+	var a = 10;
+	b=20;
+}
+hello("Kildong");
+// console.log(a);
+console.log(b);
+var contentEl = document.querySelector("#content");
+console.log(contentEl);
